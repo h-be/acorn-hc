6d9317b81d83c200b93aec7a0b07bec459acfbbb
Connor Turland <connorturland@gmail.com>
2019-08-12 17:11:09 -0400

    change up bg and box colors

--- ui-src/src/drawing/index.js
+++ ui-src/src/drawing/index.js
@@ -21,7 +21,7 @@ function render(store, canvas) {
 
   // render each goal as rectangle to the canvas/screen
   state.goals.forEach(function(goal, index) {
-    ctx.fillStyle = 'red'
+    ctx.fillStyle = '#FFF'
     ctx.fillRect(100 + 300 * index, 200, 200, 64)
     // render text
     ctx.fillStyle = 'black'
