808cfdcd163a464a38867d0841f912515960bc09
Connor Turland <connorturland@gmail.com>
2019-08-15 10:15:52 -0400

    remove semicolons

--- ui-src/src/drawing/index.js
+++ ui-src/src/drawing/index.js
@@ -10,18 +10,18 @@ import drawGoalCard from './drawGoalCard'
 
 function setupCanvas(canvas) {
   // Get the device pixel ratio, falling back to 1.
-  var dpr = window.devicePixelRatio || 1;
+  var dpr = window.devicePixelRatio || 1
   // Get the size of the canvas in CSS pixels.
-  var rect = canvas.getBoundingClientRect();
+  var rect = canvas.getBoundingClientRect()
   // Give the canvas pixel dimensions of their CSS
   // size * the device pixel ratio.
-  canvas.width = rect.width * dpr;
-  canvas.height = rect.height * dpr;
-  var ctx = canvas.getContext('2d');
+  canvas.width = rect.width * dpr
+  canvas.height = rect.height * dpr
+  var ctx = canvas.getContext('2d')
   // Scale all drawing operations by the dpr, so you
   // don't have to worry about the difference.
-  ctx.scale(dpr, dpr);
-  return ctx;
+  ctx.scale(dpr, dpr)
+  return ctx
 }
 
 // `store` is a redux store
