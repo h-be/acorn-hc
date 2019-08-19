241351b22e4316bff8233902da83a166f33b7fea
Connor Turland <connorturland@gmail.com>
2019-08-19 18:09:11 -0400

    move Help to own component and allow show/hide

--- ui-src/src/components/App.js
+++ ui-src/src/components/App.js
@@ -1,16 +1,14 @@
 import React from 'react'
 
 import GoalForm from './GoalForm'
+import Help from './Help'
 
 export default function App() {
   // The following instructions are just placeholder
   return (
     <div>
-      <p className='instructions'>Hold down 'g' and click anywhere to start creating a Goal</p>
-      <p className='instructions'>Click on a node to select it</p>
-      <p className='instructions'>With a node selected, hold down 'g' and click anywhere to create a connected (child) Goal</p>
-      <p className='instructions'>Press 'Esc' to close the Goal creator and deselect Goals</p>
       <GoalForm />
+      <Help />
     </div>
   )
 }
