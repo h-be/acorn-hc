d537e968e42f995f1704675cd92960206be09e10
hector0513 <hectorvasquez05@hotmail.com>
2019-12-19 18:13:00 -0400

    Update zomes/holo_acorn/code/src/lib.rs
    Co-Authored-By: Connor Turland <connorturland@gmail.com>

--- zomes/holo_acorn/code/src/lib.rs
+++ zomes/holo_acorn/code/src/lib.rs
@@ -218,7 +218,7 @@ mod holo_acorn {
                     hdk::EntryValidationData::Create{entry,validation_data}=>{
                         let agent_address = &validation_data.sources()[0];
                         if entry.address!=agent_address.to_string() {
-                            Err("only the same agent can create your profile".into())
+                            Err("only the same agent as the profile is about can create their profile".into())
                         }else {Ok(())}
                     },
                     hdk::EntryValidationData::Modify{
