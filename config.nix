605cf2755ac28c2d0b623caf122b07df6336c512
Connor Turland <connorturland@gmail.com>
2020-03-27 14:43:56 -0400

    fix release command and bump v

--- config.nix
+++ config.nix
@@ -67,8 +67,8 @@ echo "All finished!!!"
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.3.0";
-   previous = "0.2.1";
+   current = "0.3.1";
+   previous = "0.3.0";
   };
 
   github = {
--- nix/release/default.nix
+++ nix/release/default.nix
@@ -7,11 +7,13 @@ let
  ''
  set -euxo pipefail
  export profiles_artifact='./dnas/profiles/dist/profiles.dna.json'
+ export profiles_artifact_name='profiles.dna.json'
  export projects_artifact='./dnas/projects/dist/projects.dna.json'
+ export projects_artifact_name='projects.dna.json'
  export tag=''${CIRCLE_TAG:-${tag}}
  acorn-package
- github-release upload --file "$profiles_artifact" --owner ${config.release.github.owner} --repo ${config.release.github.repo} --tag $tag --name $artifact --token $GITHUB_DEPLOY_TOKEN
- github-release upload --file "$projects_artifact" --owner ${config.release.github.owner} --repo ${config.release.github.repo} --tag $tag --name $artifact --token $GITHUB_DEPLOY_TOKEN
+ github-release upload --file "$profiles_artifact" --owner ${config.release.github.owner} --repo ${config.release.github.repo} --tag $tag --name $profiles_artifact_name --token $GITHUB_DEPLOY_TOKEN
+ github-release upload --file "$projects_artifact" --owner ${config.release.github.owner} --repo ${config.release.github.repo} --tag $tag --name $projects_artifact_name --token $GITHUB_DEPLOY_TOKEN
  '';
 in
 {
