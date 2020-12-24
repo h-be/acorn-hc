109c7576beaf3acd53b9d80dc9239b81c8e9964a
Connor Turland <connorturland@gmail.com>
2020-12-24 14:18:35 -0500

    bump version number

--- config.nix
+++ config.nix
@@ -67,8 +67,8 @@ echo "All finished!!!"
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.4.1";
-   previous = "0.4.0";
+   current = "0.4.2";
+   previous = "0.4.1";
   };
 
   github = {
